cbb995d8b4ecc17ae0e6405a9699f1bddd4e420e
natieq <natieq@briit.co.id>
2025-09-02 15:24:31 +0700

    fixing admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import StockScrenner from './Pages/StockScreener';
+import AdminPage from './Pages/admin/AdminPage';
+import Sidebar from '../../telmi-vite/src/layouts/Sidebar';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<StockScrenner />} />
+       <Route path="/admin" element={<AdminPage />} />
+      <Route path="/sidebar" element={<Sidebar />} />
     </Routes>
 
   );
