9ea9f497797f3c30640ae164dc2a3becc82d5f07
natieq <natieq@briit.co.id>
2025-09-06 18:40:07 +0700

    add menu bandarmologi 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-
 import StockScrenner from './Pages/StockScreener';
 import AdminPage from './Pages/admin/AdminPage';
-import Sidebar from './Components/Sidebar';
 import BandarmologiPage from './Pages/BandarmologiPage';
 
 function App() {
@@ -13,7 +11,6 @@ function App() {
        <Route path="/admin" element={<AdminPage />} />
       <Route path="/bandarmologi" element={<BandarmologiPage />} />
     </Routes>
-
   );
 }
 
